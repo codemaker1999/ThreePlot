06bf3452d175ce2dfc9827d358e0d3b05a01b6c6
John Meade <jon.meadee@gmail.com>
2014-11-19 21:29:33 -0500

    refactored test page

--- test/main.js
+++ test/main.js
@@ -1,99 +1,93 @@
-var plots = [];
+function addElem (name, n) {
+    var par = document.getElementById("plotlist");
+    var d = document.createElement("div");
+    d.className = "listElem";
+    d.onclick = (function(n) {
+        return function (e) {
+            run(n);
+        };
+    })(n);
+    d.innerHTML = name;
+    par.appendChild(d);
+}
 
-/*\
-|*| Lines
-\*/
+function run (n) {
+    ThreePlot.activePlots = [];
+    document.getElementById('plot').innerHTML = '';
+    ThreePlot.plot([ plots[n] ], document.getElementById("plot"));
+}
 
-// standard line plot
-var square = {
-    "color": "#ee1155",
-    "type": "lineplot",
-    "data": [ [0,0,0], [1,0,0], [1,0,1], [0,0,1], [0,0,0] ]
-};
-plots.push(square);
-
-// Parametric plot animation
-var animHelix = {
-    "color": "#55ee11",
-    "type": "lineplot",
-    "animated": true,
-    "lineLength": 10000, // buffered geometry is fixed size
-    "xyz": [0,1,0], // initial condition
-    "step": function () {
-        var t = this.t;
-        var p = [Math.sin(t), Math.cos(t), t/10];
-        this.t += this.dt;
-        this.t = this.t % 100;
-        return p;
-    },
-    "t": 0,
-    "dt": 1/50
-};
-plots.push(animHelix);
-
-// 3d animation
-var animWireSphere = {
-    "type": "lineplot",
-    "animated": true,
-    "lineLength": 300, // buffered geometry is fixed size
-    "xyz": [0,0,0], // initial condition
-    "step": function () {
-        var phi   = 2*Math.PI*Math.random(),
-            theta = Math.PI*Math.random(),
-            x     = Math.cos(phi)*Math.sin(theta),
-            y     = Math.sin(phi)*Math.sin(theta),
-            z     = Math.cos(theta);
-        return [x,y,z];
-    }
-};
-plots.push(animWireSphere);
-
-/*\
-|*| Surfaces
-\*/
-
-// add data for z=sin(x)+cos(y), a sinusoidal blanket
-var sineBlanket = [];
-for (var i = 0; i < 50; i+=0.4) {
-    var row = [];
-    for (var j = 0; j < 50; j+=0.4) {
-        row.push( Math.sin(i) + Math.cos(j) );
+function sineBlanket(minx,miny,maxx,maxy,step) {
+    var sb = [];
+    for (var i = minx; i < maxx; i+=step) {
+        var row = [];
+        for (var j = miny; j < maxy; j+=step) {
+            row.push( Math.sin(i) + Math.cos(j) );
+        };
+        sb.push( row );
     };
-    sineBlanket.push( row );
-};
+    return sb
+}
 
-var surf = {
-    "type": "surfaceplot",
-    // min and max values for independent variables
-    "min_i": -10,
-    "max_i": 10,
-    "min_j": -10,
-    "max_j": 10,
-    // define dependent variable axis
-    "up": [0,0,1],
-    "data": sineBlanket
-};
-plots.push(surf);
+var plots = [
+    // ----------------------------------------------------------
+    {
+        "label": "square",
+        "color": "#ee1155",
+        "type": "lineplot",
+        "data": [ [0,0,0], [1,0,0], [1,0,1], [0,0,1], [0,0,0] ]
+    },
+    // ----------------------------------------------------------
+    {
+        "label": "parametric spiral",
+        "color": "#55ee11",
+        "type": "lineplot",
+        "animated": true,
+        "lineLength": 10000, // buffered geometry is fixed size
+        "xyz": [0,1,0], // initial condition
+        "step": function () {
+            var t = this.t;
+            var p = [Math.sin(t), Math.cos(t), t/10];
+            this.t += this.dt;
+            this.t = this.t % 100;
+            return p;
+        },
+        "t": 0,
+        "dt": 1/50
+    },
+    // ----------------------------------------------------------
+    {
+        "label": "random 3D line sphere",
+        "type": "lineplot",
+        "animated": true,
+        "lineLength": 300, // buffered geometry is fixed size
+        "xyz": [0,0,0], // initial condition
+        "step": function () {
+            var phi   = 2*Math.PI*Math.random(),
+                theta = Math.PI*Math.random(),
+                x     = Math.cos(phi)*Math.sin(theta),
+                y     = Math.sin(phi)*Math.sin(theta),
+                z     = Math.cos(theta);
+            return [x,y,z];
+        }
+    },
+    // ----------------------------------------------------------
+    {
+        "label": "sine blanket",
+        "type": "surfaceplot",
+        // min and max values for independent variables
+        "min_i": -10,
+        "max_i": 10,
+        "min_j": -10,
+        "max_j": 10,
+        // define dependent variable axis
+        "up": [0,0,1],
+        "data": sineBlanket(-10,-10,10,10,0.5)
+    }
+    // ----------------------------------------------------------
+];
 
-/*\
-|*| Setup
-\*/
-
-var n  = plots.length;
-var nh = Math.ceil(Math.sqrt(n));
-var nw = Math.ceil(Math.sqrt(n));
-var w  = $(document).width() / nw;
-var h  = $(document).height() / nh;
-
-for (var i = 0; i < n; i++) {
+for (var i = 0; i < plots.length; i++) {
     var p = plots[i];
-    $("#plots").append('<div id="plot'+i+'" class="container"></div>');
-    $("#plot"+i).css({
-        "width":    "" + w + "px",
-        "height":   "" + h + "px",
-        "position": "absolute",
-        "left":     "" + w * (i % nw) + "px",
-        "top":      "" + h * Math.floor(i/nw) + "px",
-    });
-    ThreePlot.plot( [ p ], $("#plot"+i)[0] );
+    addElem(p.label, i);
 };
